12be523596b1b048fa7f662dc439b07d69b40daf
SohomCh <118652491+SohomCh@users.noreply.github.com>
2024-10-21 15:07:17 +0530

    Add files via upload

---
+++ todo react/frontend/reactapptodo/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CreateTodo } from './CreateTodo'
+
+const App = () => {
+  return (
+    <div>Hii There
+    <CreateTodo ></CreateTodo>
+ 
+ 
+ 
+ </div> )
+}
+
+export default App
